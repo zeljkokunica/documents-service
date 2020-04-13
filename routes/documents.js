5f626ff7eecad3a7da18aa1802f451a1c1cb2505
zeljkokunica <zeljko.kunica@gmail.com>
2020-04-13 20:43:49 +0200

    error pass

--- routes/documents.js
+++ routes/documents.js
@@ -8,14 +8,26 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/:templateName/render', async (req, res, next) => {
-    const body = req.body;
-    const pdf = await renderer.renderToPdf(
-        body.company,
-        req.params.templateName,
-        body.model
-    );
-    res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
-    res.send(pdf);
+    try {
+        const body = req.body;
+        const pdf = await renderer.renderToPdf(
+            body.company,
+            req.params.templateName,
+            body.model,
+            {
+                format: "A4",
+                displayHeaderFooter: true,
+                headerTemplate: "<div style='width: 100%; text-align: right'><span style='font-size: 24px;' class='pageNumber'></span><span style='font-size: 24px;'>/</span><span style='font-size: 24px;'class='totalPages'></span></div> ",
+                margin: {
+                    top: "60px"
+                }
+            }
+        );
+        res.set({'Content-Type': 'application/pdf', 'Content-Length': pdf.length});
+        res.send(pdf);
+    } catch (e) {
+        next(e);
+    }
 });
 
 
